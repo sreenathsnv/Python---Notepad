afa729eff81f45ffeaa7ae0c8e9a1cb7df5c0e2d
Sreenath V <sreenathsnvaltpb@gmail.com>
2022-06-04 22:15:43 +0530

    Text area is craeted

--- notepad.py
+++ notepad.py
@@ -7,6 +7,9 @@ root = Tk()
 
 root.geometry("500x500")
 root.title('Notepad')
+# ============================Functions=================
+
+
 
 # ========================== Tool area =====================
 tool = Frame(master = root,background = '#bdf2d5',height=50)
@@ -16,7 +19,7 @@ openbt = Button(master=tool , background='#112b3c',activebackground='#205375',te
 openbt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
 
 savebt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'SAVE', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
-savebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
+savebt.pack(ipadx=1,pady= 10,padx=15,side=LEFT)
 
 Voicebt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'VOICE..', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
 Voicebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
@@ -24,7 +27,9 @@ Voicebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
 # ========================== Text area =====================
 
 text_frame = Frame(master = root,background = 'yellow')
-tool.pack(fill=BOTH)
+text_frame.pack(fill=BOTH)
 
+note  =Text(master=text_frame,font=('Arial',10,'bold'))
+note.pack(fill=BOTH)
 
 root.mainloop()
