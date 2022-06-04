76b4924a5aaaec955a1c5408b29c56a5bb2744ff
Sreenath V <sreenathsnvaltpb@gmail.com>
2022-06-04 22:06:28 +0530

    Created Buttons

--- notepad.py
+++ notepad.py
@@ -8,7 +8,23 @@ root = Tk()
 root.geometry("500x500")
 root.title('Notepad')
 
+# ========================== Tool area =====================
+tool = Frame(master = root,background = '#bdf2d5',height=50)
+tool.pack(fill=X)
 
+openbt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'OPEN', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+openbt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
+
+savebt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'SAVE', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+savebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
+
+Voicebt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'VOICE..', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+Voicebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
+
+# ========================== Text area =====================
+
+text_frame = Frame(master = root,background = 'yellow')
+tool.pack(fill=BOTH)
 
 
 root.mainloop()
