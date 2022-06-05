7aad5e8736a974e6223ca5b6aae7ab5c4d5a5331
Sreenath V <sreenathsnvaltpb@gmail.com>
2022-06-05 09:54:32 +0530

    Exit Button created

--- notepad.py
+++ notepad.py
@@ -1,8 +1,9 @@
 
 # importing Dependencies 
 
-from doctest import master
 from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import FileDialog
 
 root = Tk()
 
@@ -28,10 +29,15 @@ savebt = Button(master=tool , background='#ce9461',activebackground='#dea057',te
 savebt.pack(ipadx=1,pady= 10,padx=15,side=LEFT)
 
 Voicebt = Button(master=tool , background='#ce9461',activebackground='#dea057',text= 'VOICE..', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
-Voicebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
+Voicebt.pack(ipadx=1,pady= 10,padx=5,side=RIGHT)
+
+exitbt = Button(master=tool , background='#ce9461',activebackground='#dea057',text= 'EXIT', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+exitbt.pack(ipadx=1,pady= 10,padx=5,side=RIGHT)
 
 # ========================== Text area =====================
 
 note = Text(master= root,width=300,height = 300,bg='#e3fcbf',fg='#15133c',font=('Segoe UI',12),relief=FLAT,selectbackground='#ffe459',selectforeground='#152d35')
 note.pack(fill='both',ipadx= 15,ipady=10)
+
+
 root.mainloop()
