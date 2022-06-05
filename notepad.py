1cc7905bbffd3e1c6950da14d530919302847f76
Sreenath V <sreenathsnvaltpb@gmail.com>
2022-06-05 09:44:27 +0530

    UI modified

--- notepad.py
+++ notepad.py
@@ -1,6 +1,7 @@
 
 # importing Dependencies 
 
+from doctest import master
 from tkinter import *
 
 root = Tk()
@@ -9,27 +10,28 @@ root.geometry("500x500")
 root.title('Notepad')
 # ============================Functions=================
 
-
+def save():
+    pass
+def open():
+    pass
+def voice():
+    pass
 
 # ========================== Tool area =====================
-tool = Frame(master = root,background = '#bdf2d5',height=50)
-tool.pack(fill=X)
+tool = Frame(master = root,background = '#bdf2d5',height=70,width=300)
+tool.pack(fill=BOTH,side='top')
 
-openbt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'OPEN', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+openbt = Button(master=tool , background='#ce9461',activebackground='#dea057',text= 'OPEN', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
 openbt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
 
-savebt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'SAVE', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+savebt = Button(master=tool , background='#ce9461',activebackground='#dea057',text= 'SAVE', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
 savebt.pack(ipadx=1,pady= 10,padx=15,side=LEFT)
 
-Voicebt = Button(master=tool , background='#112b3c',activebackground='#205375',text= 'VOICE..', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
+Voicebt = Button(master=tool , background='#ce9461',activebackground='#dea057',text= 'VOICE..', fg= 'white',width= 10,height = 1,activeforeground= 'white',relief= FLAT)
 Voicebt.pack(ipadx=1,pady= 10,padx=5,side=LEFT)
 
 # ========================== Text area =====================
 
-text_frame = Frame(master = root,background = 'yellow')
-text_frame.pack(fill=BOTH)
-
-note  =Text(master=text_frame,font=('Arial',10,'bold'))
-note.pack(fill=BOTH)
-
+note = Text(master= root,width=300,height = 300,bg='#e3fcbf',fg='#15133c',font=('Segoe UI',12),relief=FLAT,selectbackground='#ffe459',selectforeground='#152d35')
+note.pack(fill='both',ipadx= 15,ipady=10)
 root.mainloop()
